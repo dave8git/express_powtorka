5dcf1d92af7906f069539da3b90e6ae736b838d3
dave8git <dave8git@gmail.com>
2023-03-05 18:17:25 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const path = require('path');
+
+const app = express(); 
+
+app.use((req, res, next) => {
+    res.show = (name) => {
+        res.sendFile(path.join(__dirname, `/views/${name}`));
+    };
+    next();
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const hbs = require('express-handlebars'); 
+
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
+
+app.get('/hello/:name', (req, res) => {
+    res.render('hello', { layout: false, name: req.params.name});
+});
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/contact', (req, res) => { 
+    res.render('contact');
+}); 
+
+app.get('/info', (req, res) => { 
+    res.render('info');
+}); 
+
+app.get('/history', (req, res) => { 
+    res.render('history');
+}); 
+
+app.post('/contact/send-message', (req, res) => {
+    const {author, sender, title, message, file } = req.body; 
+    
+    if(author && sender && title && message && file) {
+        res.render('contact', { isSent: true });
+    } 
+    else {
+        res.render('contact', { isError: true });
+    }
+}); 
+
+app.use((req, res) => {
+    res.status(404).send('404 not found ...');
+});
+
+app.listen(7000, () => {
+    console.log('Server is running on port: 7000');
+});
